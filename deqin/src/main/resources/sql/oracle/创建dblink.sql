f111b2b05eb248eeb8b3527b70fb73937768b07f
xiaolin <xiaolin@ies.com>
2017-11-29 10:20:21 +0800

    多余删除

--- deqin/src/main/resources/sql/oracle/创建dblink.sql
+++
@@ -1,6 +0,0 @@
--- Drop existing database link 
-drop database link TJ_SCHEMA0011_DBLINK;
--- Create database link 
-create database link TJ_SCHEMA0011_DBLINK
-  connect to SCHEMA0011 identified by DHCzAYhPQa
-  using '192.168.132.10:1521/tjdb';
